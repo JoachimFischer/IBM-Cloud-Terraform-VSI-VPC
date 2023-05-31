764d815bf937076360d50eee4ad9fcf89b9962f5
Joachim Fischer <joachim.fischer@de.ibm.com>
2023-05-31 13:21:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,22 +31,6 @@ resource "ibm_is_vpc_address_prefix" "prefix-1" {
 #  is_default  = true
 }
 
-resource "ibm_is_vpc_address_prefix" "prefix-2" {
-  name = "zone2-cidr-2"
-  vpc  = ibm_is_vpc.vpc1.id
-  zone = var.zone2
-  cidr = var.vpc-address-prefix-2
-#  is_default  = true
-}
-
-resource "ibm_is_vpc_address_prefix" "prefix-3" {
-  name = "zone3-cidr-3"
-  vpc  = ibm_is_vpc.vpc1.id
-  zone = var.zone3
-  cidr = var.vpc-address-prefix-3
-#  is_default  = true
-}
-
 #---------------------------------------------------------
 # Create Subnets in Zone1 / Zone2 / Zone3
 # adding public gateway to each subnet in each zone
@@ -59,18 +43,3 @@ resource "ibm_is_subnet" "server-subnet-zone1" {
   public_gateway  = ibm_is_public_gateway.pubgw-zone1.id
 }
 
-resource "ibm_is_subnet" "server-subnet-zone2" {
-  name            = "${var.vpc-name}-${var.zone2}-server"
-  vpc             = ibm_is_vpc.vpc1.id
-  zone            = var.zone2
-  ipv4_cidr_block = var.server-subnet-zone-2
- # public_gateway  = ibm_is_public_gateway.pubgw-zone2.id
-}
-
-resource "ibm_is_subnet" "server-subnet-zone3" {
-  name            = "${var.vpc-name}-${var.zone3}-server"
-  vpc             = ibm_is_vpc.vpc1.id
-  zone            = var.zone3
-  ipv4_cidr_block = var.server-subnet-zone-3
- # public_gateway  = ibm_is_public_gateway.pubgw-zone3.id
-}
