ee0055f8bfffebc4c32f15c2ab93a3d19808d73b
“Joachim <joachim.fischer@de.ibm.com>
2023-06-05 15:20:21 +0200

    acl

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,13 +1,13 @@
 #---------------------------------------------------------
 # Create Webapptier Security Group & Rules
 #---------------------------------------------------------
-resource "ibm_is_security_group" "server-securitygroup" {
-  name = "${var.vpc-name}-server-securitygroup"
-  vpc  = ibm_is_vpc.vpc1.id
-}
+# resource "ibm_is_security_group" "server-securitygroup" {
+#   name = "${var.vpc-name}-server-securitygroup"
+#   vpc  = ibm_is_vpc.vpc1.id
+# }
 
 resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp" {
-  group      = ibm_is_security_group.server-securitygroup.id
+  group     = ibm_is_vpc.vpc1.default_security_group.id
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
@@ -18,7 +18,7 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp" {
 }
 
 resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp1" {
-  group      = ibm_is_security_group.server-securitygroup.id
+  group     = ibm_is_vpc.vpc1.default_security_group.id
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
@@ -29,7 +29,7 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp1" {
 }
 
 resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp2" {
-  group      = ibm_is_security_group.server-securitygroup.id
+  group     = ibm_is_vpc.vpc1.default_security_group.id
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
@@ -40,7 +40,7 @@ resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-tcp2" {
 }
 
   resource "ibm_is_security_group_rule" "server-securitygroup-rule-in-icmp" {
-  group      = ibm_is_security_group.server-securitygroup.id
+  group     = ibm_is_vpc.vpc1.default_security_group.id
   direction  = "inbound"
   ip_version = "ipv4"
   remote     = "0.0.0.0/0"
